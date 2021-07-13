185e43f8e565a518d7c9e10d0c2c6c1ea0c741f5
s4u10 <36417793+s4u10@users.noreply.github.com>
2021-07-13 15:25:27 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,191 @@
+function clicou() {
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+}
+
+function redirecionar() {
+    window.open("https://digitalinnovation.one");
+    window.location.href = "https://digitalinnovation.one";
+}
+
+function trocar(elemento) {
+    // document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse";
+    elemento.innerHTML = "Obrigado por passar o mouse";
+    //alert("trocar texto");
+}
+
+function voltar(elemento) {
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load() {
+    alert("Pagina carregada");
+}
+
+function funcaoChange(elemento) {
+    console.log(elemento.value)
+}
+
+//function clicou() {
+// alert("Öbrigado por clicar ");
+//}
+
+/*function soma(n1, n2) {
+return n1 + n2;
+}
+
+var validar = 0;
+
+function validaIdade(idade) {
+    validar;
+    if (idade >= 18) {
+        validar = true
+    } else {
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade");
+validaIdade(idade);
+console.log(validar);
+*/
+
+//var idade = prompt("Qual sua idade");
+//console.log(validaIdade(idade));
+//alert(soma(5, 10));
+
+/*function soma(n1, n2) {
+    return n1 + n2;
+}
+
+function validaIdade(idade) {
+    var validar;
+    if (idade >= 18) {
+        validar = true
+    } else {
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade");
+//var idade = prompt("Qual sua idade");
+console.log(validaIdade(idade));
+*/
+
+/*
+function soma(n1, n2) {
+    return n1 + n2;
+}
+
+function setReplace(frase, nome, novo_nome) {
+    return frase.replace(nome, novo_nome)
+}
+
+alert(soma(5, 10));
+alert(setReplace("Vai Japao", "Japao", "Brasil"));
+*/
+
+
+/*console.log("Hoje é" + getDate() + "/
+" (getMonth() + 1));*/
+/*var list = [nome: "rasa", "cravo", "tulipa", "margarida"]
+console.log(lista[2]);*/
+/*
+    var d = new Date();
+    alert(d);
+    */
+
+/*var count;
+for (count = 0; count <= 5; count++) {
+    alert(count);
+}; * /
+
+
+/*var count = 0;
+while (count <= 5) {
+    console.log(count);
+    alert(count);
+    count++;
+};*/
+
+
+/*var count = 0;
+while (count <= 5) {
+    console.log(count);
+    count = count + 1;
+};
+*/
+/*
+var idade = prompt("Qual sua idade");
+
+var idade = prompt;
+if (idade >= 18) {
+    alert("Mairo de idade");
+} else {
+    alert("Menor de idade");
+};
+*/
+
+/*var idade = 18;
+    if (idade >= 18) {
+        alert("mairo de idade");
+    } else {
+        alert("menor de idade");
+    };
+    */
+/*
+var frutas = [{nome: "maça",cor: "vermelha"}, {nome: "uva",cor: "roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+/*
+var fruta = { nome: "maça", cor: "vermelha" }
+console.log(fruta.nome);
+alert(fruta.cor); */
+
+//var lista = ["maça", "Pera", "Laranja"];
+//lista.push("uva");
+
+
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" | "));
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+//console.log(lista[0]);
+//console.log(lista.toString()[0]);
+
+//console.log(lista.reverse());
+
+//console.log(lista.length);
+
+//lista.push("uva");
+//lista.pop();
+//lista.push("uva");
+//console.log(lista);
+
+
+//console.log(lista[1]);
+//alert(lista[1]);
+//var nome = "Saulo Filho";
+//var idade = 43;
+//var idade2 = 10;\
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + "anos");
+//alert("Meu Primeiro JS");
+//alert(idade + idade2)
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(n1 * n2);
+//console.log(frase.toLowerCase());
+//console.log(frase.toUpperCase());
+//console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
